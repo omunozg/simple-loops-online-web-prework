d0f6d65c1c42a4ff906e03a5b76324f66d75c00e
Oscar Munoz <omunozg@emory.edu>
2019-03-10 04:52:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,7 +25,7 @@ end
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 1
-  while counter >= number_of_times do
+  while counter < number_of_times do
    puts(phrase)
    counter = counter + 1 
 end
